a378c52b7c163811df5727cf76acfeef90e0d307
Eduardo Iglesias <eriglesias@ufl.edu>
2019-09-20 19:39:27 -0400

    allow users to input number of players and display blinds at appropriate times

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -16,10 +16,12 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	defer close()
+
+	game := poker.NewTexasHoldem(poker.BlindAlerterFunc(poker.StdOutAlerter), store)
+	cli := poker.NewCLI(os.Stdin, os.Stdout, game)
 
 	fmt.Println("Let's play poker")
 	fmt.Println("Type {Name} wins to record a win")
-	poker.NewCLI(store, os.Stdin, poker.BlindAlerterFunc(poker.StdOutAlerter)).PlayPoker()
+	cli.PlayPoker()
 }
